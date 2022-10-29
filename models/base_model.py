083bfbd3a55ce950847b0e4be128cc67acfd22db
yrncollo <ngenocollins599@gmail.com>
2022-10-29 12:02:35 +0300

    3 basemodel: updates the public instance attribute updated_at with the current datetime

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,15 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
     def __str__(self):
         """should print: [<class name>] (<self.id>) <self.__dict__>"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
             models.storage.new(self)
+
+    def save(self):
+        """updates the public instance attribute updated_at with the
+            current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
