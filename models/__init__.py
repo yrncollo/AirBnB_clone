c2bd9de9f76899b787b67fbd744e7e488aadcb2e
yrncollo <ngenocollins599@gmail.com>
2022-10-30 11:51:50 +0300

    updated __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-from models import base_model
+from models import base_model, state
+from models.engine import file_storage
 
 
 BaseModel = base_model.BaseModel
+State = state.State
