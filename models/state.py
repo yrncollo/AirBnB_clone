c6a678d1a72deaa33efdc7bf09fcb12b5d2f2eea
yrncollo <ngenocollins599@gmail.com>
2022-10-30 11:40:22 +0300

    written doc of the class state

--- models/state.py
+++ models/state.py
@@ -1,5 +1,12 @@
+#!/usr/bin/python3
+"""
+class that inherits from base_model
+"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
+    """
+    State model class
+    """
     name = ""
