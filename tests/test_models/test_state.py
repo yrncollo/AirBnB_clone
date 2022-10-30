a29908e55648a5c4262e27abb20eecfaf6ff54eb
yrncollo <ngenocollins599@gmail.com>
2022-10-30 22:19:17 +0300

    made corrections to test state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,64 +1,55 @@
 #!/usr/bin/python3
-"""
-This contain all tests for state
-"""
+'''
+    Contain tests for the state module.
+'''
 import unittest
-import inspect
 import pep8
+import inspect
 from models import state, BaseModel
 from models.state import State
 
 
 class TestStateDocs(unittest.TestCase):
-    """
-    To check for documentation
-    """
+    """Tests to check the documentation and style of State class"""
     @classmethod
     def setUpClass(cls):
-        cls.base_func = inspect.getmembers(State, inspect.isfunction)
+        """Set up for the doc tests"""
+        cls.base_funcs = inspect.getmembers(State, inspect.isfunction)
 
     def test_pep8_style_base(self):
-        """
-        To tests for pep8 styling has been met
-        """
+        """Test that models/state.py conforms to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/state.py'])
         self.assertEqual(result.total_errors, 0,
-                         "Found code style errors and warnings.")
+                         "Found code style errors (and warnings).")
 
-    def test_pep8_style_Base(self):
-        """
-        To tests for pep8 styling has been met
-        """
+    def test_pep8_style_base(self):
+        """Test that tests/test_models/test_state.py conforms to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/test_state.py'])
+        result = pep8style.check_files([
+            'tests/test_models/test_state.py'
+        ])
         self.assertEqual(result.total_errors, 0,
-                         "Found code style errors and warnings.")
+                         "Found code style errors (and warnings).")
 
     def test_module_docstring(self):
-        """
-        Test for module docstring
-        """
+        """Tests for the module docstring"""
         self.assertTrue(len(state.__doc__) >= 1)
 
     def test_class_docstring(self):
-        """
-        Test for state class docstring
-        """
+        """Tests for the State class docstring"""
         self.assertTrue(len(State.__doc__) >= 1)
 
-    def test_func_docstings(self):
-        """
-        Test for docstring in all the functions
-        """
-        for func in self.base_func:
+    def test_func_docstrings(self):
+        """Tests for the presence of docstrings in all functions"""
+        for func in self.base_funcs:
             self.assertTrue(len(func[1].__doc__) >= 1)
 
 
 class TestState(unittest.TestCase):
-    """
-    Testing State class
-    """
+    '''
+        Test the State class.
+    '''
 
     def setUp(self):
         """ seting up BaseModel instance to be used for tests """
