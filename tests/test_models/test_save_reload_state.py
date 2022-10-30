1aadf45f737a56d385ee78539857e49cfb1a7be3
yrncollo <ngenocollins599@gmail.com>
2022-10-30 22:25:42 +0300

    test save reload state

---
+++ tests/test_models/test_save_reload_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from models import storage
+from models.base_model import BaseModel
+from models.state import State
+
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new State --")
+my_state = State()
+my_state.name = "Nevada"
+my_state.save()
+print(my_state)
+
+print("-- Create a new State 2 --")
+my_state2 = State()
+my_state2.name = "Disney"
+my_state2.save()
+print(my_state2)
