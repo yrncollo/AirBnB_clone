bde8fc06dd43351ee4f77fa91108609fa181f086
yrncollo <ngenocollins599@gmail.com>
2022-10-30 22:01:29 +0300

    rewrite the __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,16 @@
-#!/usr/bin/python4
-from models import base_model, state, city, amenity, place, review, user
+#!/usr/bin/python3
+from models import base_model, user, state, city, amenity, place, review 
 from models.engine import file_storage
 
-
 BaseModel = base_model.BaseModel
-State = state.State
-storage = file_storage.FileStorage()
-storage.reload()
-City = city.City
 User = user.User
+State = state.State
+City = city.City
 Amenity = amenity.Amenity
 Place = place.Place
 Review = review.Review
 classes = {"BaseModel": BaseModel, "User": User, "State": State,
            "City": City, "Amenity": Amenity, "Place": Place,
            "Review": Review}
+storage = file_storage.FileStorage()
+storage.reload()
