853b913444c74d6ecbd82b6bbb94674f9a0b75f3
yrncollo <ngenocollins599@gmail.com>
2022-10-30 14:13:24 +0300

    classes that inherit from BaseModel: "city"

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this module supplies city class
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    Class City
+    """
+    state_id = ""
+    name = ""
