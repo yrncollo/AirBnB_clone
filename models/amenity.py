516b1c383fa13e6645754ce361b5ab8ec6c9bb13
yrncollo <ngenocollins599@gmail.com>
2022-10-30 15:32:45 +0300

    amenity

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+    Module that supplies Amenity class
+'''
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''
+        Definition of the Amenity class
+    '''
+    name = ""
