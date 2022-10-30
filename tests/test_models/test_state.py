182f728733e520f5c62d8d286668a5c48708e8d6
yrncollo <ngenocollins599@gmail.com>
2022-10-30 12:46:15 +0300

    unittest for docstring for functions

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,14 +2,14 @@
 """
 This contain all tests for state
 """
-import untittest
+import unittest
 import inspect
 import pep8
 from models import state, BaseModel
 from models.state import State
 
 
-class TestStateDocs(untittest.TestCase):
+class TestStateDocs(unittest.TestCase):
     """
     To check for documentation
     """
@@ -25,3 +25,31 @@ class TestStateDocs(untittest.TestCase):
         result = pep8style.check_files(['models/state.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors and warnings.")
+
+    def test_pep8_style_Base(self):
+        """
+        To tests for pep8 styling has been met
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_models/test_state.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors and warnings.")
+
+    def test_module_docstring(self):
+        """
+        Test for module docstring
+        """
+        self.assertTrue(len(state.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """
+        Test for state class docstring
+        """
+        self.assertTrue(len(State.__doc__) >= 1)
+
+    def test_func_docstings(self):
+        """
+        Test for docstring in all the functions
+        """
+        for func in self.base_func:
+            self.assertTrue(len(func[1].__doc__) >= 1)
