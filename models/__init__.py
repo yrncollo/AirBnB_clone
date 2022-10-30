706f98ad829f77565ead1f70470da4a22c765742
yrncollo <ngenocollins599@gmail.com>
2022-10-30 14:24:13 +0300

    added city module

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-from models import base_model, state
+from models import base_model, state, city
 from models.engine import file_storage
 
 
 BaseModel = base_model.BaseModel
 State = state.State
+storage = file_storage.FileStorage()
+storage.reload()
+City = city.City
